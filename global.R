67c68e346d5b562b2907be0378403202c61a0533
mostly-harmless <mostly.harmless.71@gmail.com>
2014-02-02 10:50:22 -0800

    remove includeCSS

--- global.R
+++ global.R
@@ -1,5 +1,4 @@
 html_list <- function(vars, id) {
-
   hl <- paste0("<ul id=\'",id,"\' class='stab'>")
   for(i in vars) hl <- paste0(hl, "<li class='ui-state-default stab'><span class='label'>",i,"</span></li>")
   paste0(hl, "</ul>")
@@ -8,7 +7,7 @@ html_list <- function(vars, id) {
 returnOrder <- function(inputId, vars) {
   tagList(
     singleton(tags$head(tags$script(src = 'js/sort.js'))),
-    singleton(includeCSS("www/sort.css")),
+    singleton(tags$head(tags$link(rel = 'stylesheet', type = 'text/css', href = 'sort.css'))),
     HTML(html_list(vars, inputId)),
     tags$script(paste0("$(function() {$( '#",inputId,"' ).sortable({placeholder: 'ui-state-highlight'}); $( '#",inputId,"' ).disableSelection(); });"))
   )
