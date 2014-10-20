a27ecd8c62f3a84ba8a6d6156ef48dd878197c69
mostly-harmless <vnijs@ucsd.edu>
2014-10-20 13:05:09 -0700

    Resolved jquery version conflict

--- ui.R
+++ ui.R
@@ -1,23 +1,26 @@
 shinyUI(
   pageWithSidebar(
-  
     headerPanel('Sortable list'),
-    
     sidebarPanel(
-      tags$head(tags$script(src = "js/jquery-ui.min.js")),
+      includeScript("www/js/jquery-ui.custom.min.js"),
       wellPanel(
         uiOutput('sortable_rui')
       ),
       wellPanel(
         uiOutput('sortable2_rui')
-      )
+      ),
+      sliderInput("someSlider", "Some Slider", min=1, max=5, value=1),
+      helpText("Note: If the slider above is broken there is probably a
+               conflict between the version of jquery used by
+               Shiny and the version used for Sortable (v1.10.4).")
     ),
-    
+
     mainPanel(
       tableOutput('showData'),
       verbatimTextOutput('showorder'),
       tableOutput('showData2'),
       verbatimTextOutput('showorder2')
+
     )
   )
 )
